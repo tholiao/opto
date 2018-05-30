941a053cf1fbd2561fe9017f9c2aaf1950ea9a90
Thomas Liao <thomasliao@berkeley.edu>
2018-05-29 23:38:24 -0700

    Refactored EI for line length etc

--- opto/opto/acq_func/EI.py
+++ opto/opto/acq_func/EI.py
@@ -14,12 +14,15 @@ class EI(AcquisitionFunction):
     """
     Expected Improvement
     """
+
     def __init__(self, model, logs, parameters={}):
         super(EI, self).__init__(model=model, parameters=parameters)
         self.target = parameters.get('target', None)
-        self.bias = parameters.get('bias', 0.01)  # bias. default value according to [Lizotte]
+        # The default bias is according to [Lizotte]
+        self.bias = parameters.get('bias', 0.01)
         if (self.target is None) and (logs is not None):
-            self.target = logs.get_best_objectives() - self.bias  # best observation so far
+            # we store the best observation sof ar
+            self.target = logs.get_best_objectives() - self.bias
 
     def set_target(self, target):
         self.target = target
@@ -27,20 +30,25 @@ class EI(AcquisitionFunction):
     def evaluate(self, parameters):
         mean, var = self._m.predict(parameters.T)
         assert np.all(var >= 0), 'Variance can never be negative!'
+
         s = np.sqrt(var)
         s[s == 0] = np.spacing(1)  # To avoid numerical instabilities
         z = (self.target - mean) / s
-        # see [Jones et al., 1998] for derivation of the following
-        f = s * (z * norm.cdf(z) + norm.pdf(z))
-        # f = (eta - m - self.par) * norm.cdf(z) + s * norm.pdf(z)  # Alternative
-        f = -f  # Optimizer minimize acq.func.
+        f = s * (z * norm.cdf(z) + norm.pdf(z))  # Derivation from [Jones 1998]
+        # f = (eta - m - self.par) * norm.cdf(z) + s * norm.pdf(z) # alternative
+        f = -f  # Invert f, because the optimizer currently only minimizes.
+
+        # check for NaN and Inf errors
         assert np.all(np.invert(np.isnan(f)))
         assert np.all(np.invert(np.isinf(f)))
         return f
 
+    # TODO: can we make __init__ use this code to avoid duplication?
     def update(self, model, logs, parameters={}):
         self._m = model  # Model
         self.target = parameters.get('target', None)
-        self.bias = parameters.get('bias', 0.01)  # bias. default value according to [Lizotte]
+        # The default bias is according to [Lizotte]
+        self.bias = parameters.get('bias', 0.01)
         if self.target is None:
-            self.target = logs.get_best_objectives() - self.bias  # best observation so far
+            # we store the best observation so far
+            self.target = logs.get_best_objectives() - self.bias
